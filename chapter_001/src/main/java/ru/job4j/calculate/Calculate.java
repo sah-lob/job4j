624f315a2dd9ec9fbfa576f9e197db6cd86dac05
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-12 18:34:26 +0300

    Добавлены JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+
+/**
+* Calculate.
+*
+* @author Alexander Lobachev (sah-lob@ya.ru)
+*/
 public class Calculate{
+
+	/**
+	* Main
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 	System.out.println("Hello World");
 	}
