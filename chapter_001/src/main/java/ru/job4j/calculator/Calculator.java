1d07199c8d7a3efd7d9a4d0d27ee8e9e16088632
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-13 19:47:24 +0300

    добавлены жавадоки&

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -4,18 +4,38 @@ public class Calculator {
 
     private double result;
 
+    /**
+     * Сложение двух чисел.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
 
+    /**
+     * Вычитание двух чисел.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public void subtraction(double first, double second){
         this.result = first - second;
     }
 
+    /**
+     * Умножение двух чисел.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public void multiply(double first, double second){
         this.result = first*second;
     }
 
+    /**
+     * Деление двух чисел.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public void division(double first, double second){
         if(second !=0){
             this.result = first/second;
