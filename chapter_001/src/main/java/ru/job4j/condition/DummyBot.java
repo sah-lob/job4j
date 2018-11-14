8bc216314f9d814fcdca9ff0c762b04be10c5168
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-15 01:58:55 +0300

    Глупый бот.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ * @author Alexander Lobachev (sah-lob@ya.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, что этот вопрос известен боту и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
