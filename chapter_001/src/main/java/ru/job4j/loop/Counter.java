7d53a05ce8bfa221033f16342fca5761517ecad9
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-15 20:42:17 +0300

    Сумма максимальных чисел в диапазоне.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+
+/**
+ * Сумма четных чисел в диапазоне.
+ */
+public class Counter {
+
+    /**
+     * Сумма всех четных чисел в диапазоне.
+     * @param start - начало диапазона.
+     * @param finish - конец диапазона.
+     * @return - сумма чисел в диапазоне.
+     */
+    public int add(int start, int finish) {
+
+        int summ = 0;
+
+        for (int i = start; i <= finish ; i++) {
+            summ = i%2==0 ? summ+i : summ ;
+        }
+        return summ;
+    }
+}
