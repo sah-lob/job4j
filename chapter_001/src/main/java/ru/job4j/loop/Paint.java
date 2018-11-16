59df5519d8c29638ede4b47ab28f6a1df7498f33
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-16 16:28:51 +0300

    Рефакторинг класса Paint

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,22 +1,19 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Рисуем пирамиду.
  */
 public class Paint {
 
 
-    /**
-     * Создаем строку, выводящую пирамиду.
-     * @param height высота пирамиды.
-     * @return строка.
-     */
-    public String pyramid(int height) {
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
-        for (int row = 0; row < height; row++) {
-            for (int column = 0; column < weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -25,5 +22,45 @@ public class Paint {
             screen.append(System.lineSeparator());
         }
         return screen.toString();
+    }
+
+
+    /**
+     * Создаем строку, выводящую пирамиду.
+     * @param height высота пирамиды.
+     * @return строка.
+     */
+    public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    /**
+     * Cоздаем строку, выводящую правый треугольник.
+     * @param height высота треугольника.
+     * @return строка.
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    /**
+     * Cоздаем строку, выводящую левый треугольник.
+     * @param height высота треугольника.
+     * @return строка.
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
     }
 }
