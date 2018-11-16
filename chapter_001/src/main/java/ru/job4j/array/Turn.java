8988d6fc20a6b9750fa19571b2cdcea27db1707d
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-16 17:18:07 +0300

    Разворот массива.

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * Переворот массива.
+ */
+public class Turn {
+
+    /**
+     * Переворот массива по данной формуле: a=a+b-(b=a);
+     * @param array массив, который нужно перевернуть.
+     * @return перевернутый массив.
+     */
+    public int[] back(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            array[i] = array[i] + array[array.length - 1 - i] - (array[array.length - 1 - i] = array[i]);
+        }
+        return array;
+    }
+}
