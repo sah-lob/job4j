e83e9f17433ab82dbc6d45bbe9e60045cd0c7ecc
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-16 16:39:39 +0300

    Рефакторинг класса Paint

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+/**
+ * Создание массива с квадратами чисел.
+ */
+public class Square {
+
+    /**
+     *  Возвращает массив с квадратами чисел с 1 до bound
+     * @param bound - До какого числа включительно надо возвращать массив с квадратами.
+     * @return массив с квадратами чисел.
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 1; i <= bound; i++) {
+            rst[i - 1] = i * i;
+        }
+        return rst;
+    }
+}
