3714459bffffc9319d659cec741e429caddc040c
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-17 21:15:48 +0300

    Квадратный массив заполнен true или false.

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+
+
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length; i++) {
+            if (data[0][0] != data[i][i] || data[data.length - 1][0] != data[data.length - 1 - i][i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
