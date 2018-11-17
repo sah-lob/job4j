051627d81dbe2886bbafee7ff88b7c84c05991c7
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-17 20:35:03 +0300

    Доработка. Сортировка пузырьком.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -11,8 +11,8 @@ public class BubbleSort {
      * @return отсортированный массив.
      */
     public int[] sort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int num = array[j];
                     array[j] = array[j + 1];
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class BubbleSortJava {
+public class BubbleSortTest {
 
     @Test
     public void sortArrayOfThreeNumbers() {
