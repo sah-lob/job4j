be74368de505596cfe8666b3f14cbeff9ef41cc8
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-17 12:27:02 +0300

    Проверка начала слова.

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+/**
+ * Обертка над строкой.
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинается с префикса
+     */
+    public boolean startWith(String prefix) {
+
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] != data[i]) {
+                result = false;
+            }
+        }
+
+        return result;
+    }
+}
