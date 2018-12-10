a195c10be4237ffb704f7370699f0b1080fbc892
Alexander Lobachev <Sah-lob@ya.ru>
2018-12-10 14:04:39 +0300

    Исправление ошибок.

--- chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -7,9 +7,9 @@ public class ConvertMatrix2List {
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                list.add(array[i][j]);
+        for (int[] i: array) {
+            for (int num : i) {
+                list.add(num);
             }
         }
         return list;
