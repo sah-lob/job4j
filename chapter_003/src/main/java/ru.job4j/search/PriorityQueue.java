940b8f1ea4d523ae53c71dd3b601aeb41700530d
Alexander Lobachev <Sah-lob@ya.ru>
2018-12-10 12:48:12 +0300

    Исправление ошибок

--- chapter_003/src/main/java/ru.job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru.job4j/search/PriorityQueue.java
@@ -15,15 +15,25 @@ public class PriorityQueue {
     public void put(Task task) {
         if (tasks.size() == 0) {
             tasks.add(0, task);
-        }
-        for (int i = 0; i < tasks.size(); i++) {
-            if (tasks.get(i).getPriority() > task.getPriority()) {
-                tasks.add(i, task);
-                break;
+        } else {
+            boolean place = false;
+            for (int i = 0; i < tasks.size(); i++) {
+                if (tasks.get(i).getPriority() > task.getPriority()) {
+                    tasks.add(i, task);
+                    place = true;
+                    break;
+                }
+            }
+            if (!place) {
+               tasks.add((tasks.size()),task);
             }
         }
     }
     public Task take() {
         return this.tasks.poll();
+    }
+
+    public Task last() {
+        return this.tasks.peekLast();
     }
 }
--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -11,7 +11,20 @@ public class PriorityQueueTest {
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
+        queue.put(new Task("lowwwwww",8));
         Task result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
+
+    @Test
+    public void whenLowerPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        queue.put(new Task("lowwwwww",8));
+        Task result = queue.last();
+        assertThat(result.getDesc(), is("lowwwwww"));
+    }
+
 }
