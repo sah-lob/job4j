5a696b2930716f38aa93060cab51a19ddc6baa37
Alexander Lobachev <Sah-lob@ya.ru>
2019-01-22 22:33:08 +0300

    Реализация коллекции SimpleQueue.

--- chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -3,25 +3,25 @@ package ru.job4j.list;
 public class SimpleQueue<T> {
 
     private SimpleStack stack = new SimpleStack();
+    private SimpleStack simpleStack = new SimpleStack();
 
     public T poll() {
-        stack = reverse(stack);
-        T t = (T) stack.poll();
-        stack = reverse(stack);
-        return t;
+        T obj = (T) stack.poll();
+        while (obj != null) {
+            simpleStack.push(obj);
+            obj = (T) stack.poll();
+        }
+        T result = (T) simpleStack.poll();
+        obj = (T) simpleStack.poll();
+        while (obj != null) {
+            stack.push(obj);
+            obj = (T) simpleStack.poll();
+        }
+        return result;
     }
 
     public void push(T value) {
         stack.push(value);
     }
 
-    private SimpleStack reverse(SimpleStack stack) {
-        SimpleStack simpleStack = new SimpleStack();
-        T obj = (T) stack.poll();
-        while (obj != null) {
-            simpleStack.push(obj);
-            obj = (T) stack.poll();
-        }
-        return simpleStack;
-    }
 }
