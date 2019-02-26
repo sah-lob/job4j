774c53eed3efea02ff49613ad11902f437b992e4
Alexander Lobachev <Sah-lob@ya.ru>
2019-02-26 17:51:02 +0300

    Удаление запрещенных слов.

--- chapter_006/src/main/java/ru/job4j/inputoutput/InputOutput.java
+++ chapter_006/src/main/java/ru/job4j/inputoutput/InputOutput.java
@@ -1,5 +1,6 @@
 package ru.job4j.inputoutput;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputOutput {
@@ -10,6 +11,46 @@ public class InputOutput {
             result = scanner.nextInt() % 2 == 0;
         }
         return result;
+    }
+
+
+    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
+
+        int q;
+        var word = new StringBuilder();
+        while (true) {
+            q = in.read();
+            if (q == -1) {
+                abuseTest(word.toString(), abuse, out, true);
+                break;
+            } else if (q != 32) {
+                word.append((char) q);
+            } else {
+                abuseTest(word.toString(), abuse, out, false);
+                word = new StringBuilder();
+            }
+        }
+    }
+
+
+    public void abuseTest(String word, String[] abuse, OutputStream out, boolean end) throws IOException {
+        var result = true;
+        if (abuse.length != 0) {
+            for (var w : abuse) {
+                if (w.toUpperCase().equals(word.toUpperCase())) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        if (result) {
+            for (var c : word.toCharArray()) {
+                out.write(c);
+            }
+            if (!end) {
+                out.write(32);
+            }
+        }
     }
 
 }
--- chapter_006/src/test/java/ru/job4j/inputoutput/InputOutputTest.java
+++ chapter_006/src/test/java/ru/job4j/inputoutput/InputOutputTest.java
@@ -2,9 +2,7 @@ package ru.job4j.inputoutput;
 
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -28,5 +26,16 @@ public class InputOutputTest {
         }
         assertThat(inputOutput.isNumber(new InputStreamReader(System.in)), is(false));
     }
+
+    @Test
+    public void filteredOutput() throws IOException {
+        var strings = new String[]{"hello", "name"};
+        var string = "my is Alexander Lobachev. And I'm 23 year's old.";
+        var byteArrayOutputStream = new ByteArrayOutputStream();
+        var byteArrayInputStream = new ByteArrayInputStream("Hello my name is Alexander Lobachev. And I'm 23 year's old.".getBytes());
+        new InputOutput().dropAbuses(byteArrayInputStream, byteArrayOutputStream, strings);
+        assertThat(string, is(byteArrayOutputStream.toString()));
+    }
+
 
 }
