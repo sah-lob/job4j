c8781572ab2e6fc4a727ce899181e131d4028b1b
Alexander Lobachev <Sah-lob@ya.ru>
2019-05-22 22:05:31 +0300

    Динамическое перераспределение продуктов.

--- chapter_007/src/main/java/ru/job4j/foodstorage/ControllQuality.java
+++ chapter_007/src/main/java/ru/job4j/foodstorage/ControllQuality.java
@@ -1,8 +1,5 @@
 package ru.job4j.foodstorage;
-import ru.job4j.foodstorage.extension.FreeseWarehouse;
-import ru.job4j.foodstorage.extension.NewTrash;
-import ru.job4j.foodstorage.storage.Shop;
-import ru.job4j.foodstorage.storage.Warehouse;
+import ru.job4j.foodstorage.storage.Storage;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,24 +17,17 @@ public class ControllQuality {
         return (int) (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24);
     }
 
-    public void resort() {
+    public void resort(ArrayList<Storage> storage) {
+
         var foods = new ArrayList<Food>();
-        var shop = new Shop();
-        var warehouse = new Warehouse();
-        var freeseWarehouse = new FreeseWarehouse();
-        var trash = new NewTrash();
-        foods.addAll(shop.getFoods());
-        foods.addAll(warehouse.getFoods());
-        foods.addAll(freeseWarehouse.getFoods());
-        foods.addAll(trash.getFoods());
 
-        freeseWarehouse.deleteAllFoods();
-        warehouse.deleteAllFoods();
-        shop.deleteAllFoods();
-
-        freeseWarehouse.setNext(warehouse);
-        warehouse.setNext(shop);
-        shop.setNext(trash);
-        freeseWarehouse.add(foods);
+        for (int i = 0; i < storage.size(); i++) {
+            foods.addAll(storage.get(i).getFoods());
+            storage.get(i).deleteAllFoods();
+            if (i < storage.size() - 1) {
+                storage.get(i).setNext(storage.get(i + 1));
+            }
+        }
+        storage.get(0).add(foods);
     }
 }
