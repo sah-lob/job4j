15e834db1cc63ae6f5240fe6eaece8285c053e5f
Alexander Lobachev <Sah-lob@ya.ru>
2019-05-25 01:27:05 +0300

    Проблеммы с многопоточностью.

---
+++ chapter_008/src/main/java/ru/job4j/treadproblems/TestThread.java
@@ -0,0 +1,29 @@
+package ru.job4j.treadproblems;
+
+public class TestThread extends Thread{
+
+
+
+    Swapclass swapclass;
+
+    public TestThread(Swapclass swapclass) {
+        this.swapclass = swapclass;
+    }
+
+    public static void main(String[] args) {
+        var swapclass = new Swapclass();
+        var tread = new TestThread(swapclass);
+        var tread2 = new TestThread(swapclass);
+        var tread3 = new TestThread(swapclass);
+        tread.start();
+        tread2.start();
+        tread3.start();
+
+    }
+
+
+    @Override
+    public void run() {
+        swapclass.swap();
+    }
+}
