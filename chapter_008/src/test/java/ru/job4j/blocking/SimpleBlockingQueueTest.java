cd80eb15d60a621f82bee7747eaba37538344787
Alexander Lobachev <Sah-lob@ya.ru>
2019-05-27 20:34:25 +0300

    шаблон Producer Consumer

---
+++ chapter_008/src/test/java/ru/job4j/blocking/SimpleBlockingQueueTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.blocking;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+
+    SimpleBlockingQueue<Integer> queue;
+    private Integer result = 0;
+
+    @Test
+    public void whenProducerAndCustomerStartWorking() {
+
+        queue = new SimpleBlockingQueue<>();
+        var producer = new Thread() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 12; i++) {
+                    queue.offer(i);
+                }
+            }
+        };
+        var consumer = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    result = queue.poll();
+                }
+            }
+        };
+
+        producer.start();
+        consumer.start();
+        try {
+            producer.join();
+            consumer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertThat(result, is(9));
+        assertThat(queue.size(), is(2));
+
+    }
+
+}
