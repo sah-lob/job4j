971db5b64b380b5cc51114913ac2dbcc5f5753c7
Alexander Lobachev <Sah-lob@ya.ru>
2019-05-30 17:57:59 +0300

    Исправление ошибок.

--- chapter_008/src/main/java/ru/job4j/threadpool/ThreadPool.java
+++ chapter_008/src/main/java/ru/job4j/threadpool/ThreadPool.java
@@ -10,9 +10,6 @@ public class ThreadPool {
 
     private final List<Thread> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
-    private volatile boolean noShutdown = true;
-
-
 
     public ThreadPool() {
         int size = Runtime.getRuntime().availableProcessors();
@@ -25,30 +22,22 @@ public class ThreadPool {
     }
 
     public void work(Runnable job) {
-        synchronized (this.tasks) {
             this.tasks.offer(job);
-            this.tasks.notifyAll();
-            if (tasks.size() == 10) {
-                try {
-                    System.out.println("я тут");
-                    tasks.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
     public void shutdown() {
-        synchronized (this.tasks) {
-            noShutdown = false;
-            this.tasks.notifyAll();
+        while (true) {
+            if (tasks.isEmpty()) {
+                for (var t: threads) {
+                    t.interrupt();
+                }
+                break;
+            }
         }
     }
 
     public static void main(String[] args) {
         var threadPool = new ThreadPool();
-
         new Thread() {
             @Override
             public void run() {
@@ -76,22 +65,11 @@ public class ThreadPool {
     class ThreadWork implements Runnable {
         @Override
         public void run() {
-            while (noShutdown) {
-                synchronized (tasks) {
-                    while (tasks.isEmpty() && noShutdown) {
-                        try {
-                            System.out.println("Поток ждет.");
-                            tasks.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (noShutdown) {
-                        tasks.poll().run();
-                        System.out.println(String.format("Поток № %s взял в работу задачу. Осталось - %s",
-                                Thread.currentThread().getName(), tasks.size()));
-                        tasks.notify();
-                    }
+            while (!Thread.currentThread().isInterrupted()) {
+                if (!tasks.isEmpty()) {
+                    tasks.poll().run();
+                    System.out.println(String.format("Поток № %s взял в работу задачу. Осталось - %s",
+                            Thread.currentThread().getName(), tasks.size()));
                 }
             }
         }
